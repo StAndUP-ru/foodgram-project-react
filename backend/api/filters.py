c822c17305d9e00dbae02f5eaeff7f918f4d31b3
Станислав Андрющенко <StAndryushchenko@yandex.ru>
2023-05-05 23:49:36 +0300

    тест

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,13 +1,14 @@
 from django_filters.rest_framework import filters, FilterSet
 
-from recipes.models import Recipe, Tag
+# from recipes.models import Recipe, Tag
+from recipes.models import Recipe
 
 
 class RecipesFilters(FilterSet):
-    tags = filters.MultipleChoiceFilter(
-        field_name='tags__slug',
-        choices=[(tag.slug, tag.name) for tag in Tag.objects.all()]
-    )
+    # tags = filters.MultipleChoiceFilter(
+    #     field_name='tags__slug',
+    #     choices=[(tag.slug, tag.name) for tag in Tag.objects.all()]
+    # )
     is_favorited = filters.BooleanFilter(
         method='filter_is_favorited'
     )
@@ -17,6 +18,7 @@ class RecipesFilters(FilterSet):
 
     class Meta:
         model = Recipe
+        # fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
         fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
